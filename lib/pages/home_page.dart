a0f771aa47e07aef9a02381dcebe0c2541232882
vaishak.k <vaishak.k@zohocorp.com>
2019-08-18 01:49:14 +0530

    commented out the check verification alert.

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -37,7 +37,7 @@ class _HomePageState extends State<HomePage> {
     FirebaseDatabase.instance.setPersistenceEnabled(true);
     super.initState();
 
-    _checkEmailVerification();
+//    _checkEmailVerification();
 
     _todoList = new List();
     _todoQuery = _database
