7d6744299c14cea58c52102ac549c016f324edfc
vaishak.k <vaishak.k@zohocorp.com>
2019-09-07 11:05:14 +0530

    Changed the app name to Dashy.

--- lib/main.dart
+++ lib/main.dart
@@ -10,7 +10,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return new MaterialApp(
-        title: 'Flutter Login',
+        title: 'Dashy',
         debugShowCheckedModeBanner: false,
         theme: new ThemeData(
           primarySwatch: Colors.blue,
